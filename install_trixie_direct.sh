c02b3d4a2f225c614e2b6b1feffa107f0514c759
Xerolux <89860334+Xerolux@users.noreply.github.com>
2026-05-01 19:38:10 +0200

    Make patched upstream user/group creation idempotent

--- install_trixie_direct.sh
+++ install_trixie_direct.sh
@@ -77,7 +77,7 @@ trap 'on_error $? $LINENO "$BASH_COMMAND"' ERR
 # Benutzer vorbereiten und Installer im openwb-Kontext fortsetzen
 OPENWB_USER="openwb"
 OPENWB_TRIXIE_SCRIPT_URL="${OPENWB_TRIXIE_SCRIPT_URL:-https://raw.githubusercontent.com/Xerolux/openwb-trixie/main/install_trixie_direct.sh}"
-INSTALLER_VERSION="2026-05-01.10"
+INSTALLER_VERSION="2026-05-01.11"
 
 ensure_openwb_user() {
     if id "$OPENWB_USER" >/dev/null 2>&1; then
@@ -345,6 +345,8 @@ run_openwb_core_installer_noninteractive() {
         -e "s@curl -s \"https://raw.githubusercontent.com/openWB/core/master/runs/install_packages.sh\" | bash -s@bash \"$packages_script\"@g" \
         -e 's@mkdir "$OPENWBBASEDIR"@mkdir -p "$OPENWBBASEDIR"@g' \
         -e 's@sudo -u "\$OPENWB_USER" pip install -r "\${OPENWBBASEDIR}/requirements.txt"@/opt/openwb-venv/bin/pip3 install -r "\${OPENWBBASEDIR}/requirements.txt"@g' \
+        -e 's@^/usr/sbin/groupadd "\$OPENWB_GROUP"$@/usr/sbin/groupadd "\$OPENWB_GROUP" || true@g' \
+        -e 's@^/usr/sbin/useradd "\$OPENWB_USER" -g "\$OPENWB_GROUP" --create-home$@/usr/sbin/useradd "\$OPENWB_USER" -g "\$OPENWB_GROUP" --create-home || true@g' \
         "$install_script"
 
     # Fallback: fange abweichende Upstream-Varianten des pip-Aufrufs ab
